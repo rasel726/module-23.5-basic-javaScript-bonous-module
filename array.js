750fc8f60b6d7dd9c0e3d3a922656d5bf26e4ea9
rasel726 <md.rasel2726@gmail.com>
2022-08-13 01:15:18 +0600

    added

--- array.js
+++ array.js
@@ -1,15 +1,15 @@
-const products = [
-    { name: 'phone', price: 12000 },
-    { name: 'laptop', price: 32000 },
+// const products = [
+//     { name: 'phone', price: 12000 },
+//     { name: 'laptop', price: 32000 },
 
-]
+// ]
 
 // products[0];
 // products[1];
 
 // const producta = [15, 56, 87]
 const products = {
-    '0';
+    '0': 15,
     '1': 56,
     '2': 87,
 }
